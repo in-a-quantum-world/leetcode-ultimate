0ce6477ec7b6aae11cb0e1e462ba01d0d08c66fa
Rucha Agashe <ruchaagashe212@gmail.com>
2026-09-15 10:40:39 +0000

    manahcer algo

--- pallindrome.py
+++ pallindrome.py
@@ -26,7 +26,86 @@ def is_pallindrome(word):
     
     return True
         
+#lc 647, uses O(n^2) time comexplity
+#linear space complexity
+def pallindromic_substrings(s):
 
+    #expand from the centre
+    n = len(s)
+    count = 0
+
+    for centre in range(2*n - 1):
+        left = centre / 2  
+        right = left + centre%2
+
+        while left >= 0  and right < n and s[right] == s[left]: #checks if there are anyu more sols with same centre 
+            #but first adds 1 to count since we found one to enter the while loop
+            count += 1
+            right += 1
+            left -=1
+    
+    
+    return count
+
+#now for a dynamic programming approach!
+def pallindromic_substr_dp(s):
+    n = len(s)
+    count = 0
+
+    for centre in range(2*n - 1):
+        left = centre / 2
+        right = left + centre % 2
+        while left >= 0 and right < n and s[right] == s[left]:
+            count += 1
+            left -= 1
+            right += 1
+    
+    return count 
+
+
+#manacher algorithm
+#do not think i would havbe thought of this myself!
+def manacher_pallindrome_substr(s):
+    #manacher's algorithm
+    #linear timie and space complexity
+
+    n = len(s)
+    #creating a new string by inserting a hashtag between each char
+    #of original string to consider boith
+    #odd and veen length pallindromes at once
+
+    t = "#"
+
+    for i in s:
+        t += i + "#"
+    
+    n = len(t) #should be double length of s, plus 1
+
+    dp = [0] * n #creating an array of 0s, length n
+    centre = 0 #centre of current longest pallindrome
+    right = 0 #pointer to track rightmost char
+    count = 0
+
+    #iterate over the left pointer  
+    for i in range(n):
+        mirror = 2*centre - i
+        if i < right:
+            dp[i] = min(right - i, dp[mirror])
+        #attempt to expand pallindrome centred at i
+        while a < n and b >= 0 and t[a] == t[b]:
+            dp[i] += 1
+            a += 1
+            b -= 1
+        #if pallindrome centrerd at i expands past the right
+        #then the centre needs to be adjusted
+        #and the right
+        if i + dp[i] > right:
+            centre = i
+            right = i + dp[i]
+        
+        #cthen count all pallindromes found at index i
+        count += (dp[i]+1) // 2
+    return count
 
 if __name__== '__main__':
     print(is_pallindrome("RA3d 3ar"))
